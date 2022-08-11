68e51efba994c088d6fddb5318a3cc8af8025dbb
Jessica Underwood <35341395+Jlenea3@users.noreply.github.com>
2022-08-11 19:56:49 -0400

    main.js

---
+++ main.js
@@ -0,0 +1,5 @@
+import { Application } from '@splinetool/runtime';
+
+const canvas = document.getElementById('canvas3d');
+const app = new Application(canvas);
+app.load('https://prod.spline.design/KAomYjR2h0aYbQ2X/scene.splinecode');
